2b6e118234b9b8e41b7e2528291c87c40790a8d6
Anthony <acahuib@unsa.edu.pe>
2024-06-05 11:05:37 -0500

    Se agrega en view otra funcion para añadir destino

--- lab7/destinos/views.py
+++ lab7/destinos/views.py
@@ -6,3 +6,13 @@ def listar_destinos(request):
     destinos = Destino.objects.all()
     return render(request, 'destinos/listar_destinos.html', {'destinos': destinos})
 
+def anadir_destino(request):
+    if request.method == 'POST':
+        form = DestinoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_destinos')
+    else:
+        form = DestinoForm()
+    return render(request, 'destinos/form_destino.html', {'form': form})
+
