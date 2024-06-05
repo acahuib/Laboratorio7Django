5174144dd2927ef5a60f3828b3ea6d9aa080ef8a
Anthony <acahuib@unsa.edu.pe>
2024-06-05 14:05:03 -0500

    Se modifica la ruta para que este acepten los nuevos valores

--- lab7/lab7/urls.py
+++ lab7/lab7/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from destinos import views
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('destinos/', include('destinos.urls')),
     path('', views.inicio, name='inicio'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
