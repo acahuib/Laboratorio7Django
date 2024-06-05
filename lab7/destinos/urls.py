5a70a8c1191bdd8a2ec64b16ffc7dcb03bdd5f68
Anthony <acahuib@unsa.edu.pe>
2024-06-05 11:50:41 -0500

    Se modifica el nombre de añadir a anadir

--- lab7/destinos/urls.py
+++ lab7/destinos/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.listar_destinos, name='listar_destinos'),
-    path('añadir/', views.añadir_destino, name='añadir_destino'),
+    path('anadir/', views.añadir_destino, name='anadir_destino'),
     path('modificar/<int:id>/', views.modificar_destino, name='modificar_destino'),
     path('eliminar/<int:id>/', views.eliminar_destino, name='eliminar_destino'),
 ]
