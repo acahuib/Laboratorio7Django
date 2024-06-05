5f3cf41c02075f2341ab48ee9789e3df23f7a420
Anthony <acahuib@unsa.edu.pe>
2024-06-05 11:03:51 -0500

    Se crea la funcion de listar_destinos para la futura pagina web a crear de listar_destinos.html

--- lab7/destinos/views.py
+++ lab7/destinos/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Destino
+from .forms import DestinoForm
 
-# Create your views here.
+def listar_destinos(request):
+    destinos = Destino.objects.all()
+    return render(request, 'destinos/listar_destinos.html', {'destinos': destinos})
+
